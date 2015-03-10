2fec636255e1461a3394426cab4f2e52c9bb382a
Clay McClure <clay@daemons.net>
2015-03-09 22:30:37 -0400

    camera: define some Camera driver classes

---
+++ src/py/lander/drivers/camera.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# vim: set ts=4 sw=4 et:
+
+import cv2
+import numpy
+import rospy
+
+from tf.transformations import euler_from_quaternion, euler_matrix
+
+from lander.lib.position import PositionMixin
+
+
+class Camera(object, PositionMixin):
+    """
+    Abstract base camera.
+    """
+    def __init__(self, frame_size, fx, fy, frame_rate=30):
+        """
+        frame_size: size of the camera image ((width, height); pixels)
+        fx: focal length, x axis (pixels, as determined by camera calibration)
+        fy: focal length, y axis (pixels, as determined by camera calibration)
+        """
+        PositionMixin.__init__(self)
+
+        self.frame_size = frame_size
+        self.frame_rate = frame_rate
+
+        # Calculate principal point (image center) in pixel coordinates
+        cx = frame_size[0] / 2.0
+        cy = frame_size[1] / 2.0
+
+        # Build the camera's intrinsic matrix
+        # TODO: Move this out to a base class, and pass in the camera's intrinsic matrix
+        self.P = numpy.matrix((
+            (fx, 0, cx, 0),
+            (0, -fy, cy, 0),
+            (0,  0,  1, 0),
+        ))
+
+    def release(self):
+        pass
+
+    def back_project(self, u, v):
+        """
+        Project a point from (u, v) pixel coordinates to (x, y, z) world coordinates,
+        assuming the point lies on the ground plane.
+        """
+        o = self.orientation
+        q = [o.x, o.y, o.z, o.w]
+        roll, pitch, yaw = euler_from_quaternion(q)
+
+        # Compute rotation matrix, as above, but not taking the inverse because
+        # here we're mapping from camera back to world coordinates
+        R = numpy.matrix(euler_matrix(-yaw, -pitch, roll, axes="rzxy"))
+
+        # Build translation vector
+        p = self.position
+        c = numpy.matrix((p.x, p.y, p.z)).T
+
+        # Express image point as a column vector in homogeneous coordinates
+        P_i = numpy.matrix((u, v, 1)).T
+
+        # Back-project and rotate a point in image coords to a ray in world coords
+        K = self.P[:, :3]
+        R = R[:3,:3]
+        P_w = R * K.I * P_i
+
+        # NOTE: We could stop here and return camera (body-relative) coords
+
+        # Recover 3D point from homogeneous coordinates
+        # TODO: Compute the point's depth in the scene, based on camera position and orientation.
+        #       For now, we assume the vehicle is mostly level, so we can use the AGL altitude.
+        d = p.z
+        P_w = P_w * -d / P_w[2]
+
+        # Translate to world coordinates
+        P_w = P_w + c
+
+        return P_w
+
+
+class OpenCVCamera(Camera):
+    """
+    An OpenCV-compatible video capture device.
+    """
+    def __init__(self, *args, **kwargs):
+        super(OpenCVCamera, self).__init__(*args, **kwargs)
+
+        self.cap = cv2.VideoCapture(0)
+
+    def release(self):
+        """
+        Release the video capture handle.
+        """
+        self.cap.release()
+
+    def get_frame(self):
+        """
+        Return a frame captured by the camera.
+        """
+        ok, frame = self.cap.read()
+        if ok:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        return frame
+
+
+class SimulatedCamera(Camera):
+    """
+    A simulated camera.
+    """
+    def set_target(self, image_file, position, size_in_meters):
+        """
+        image_file: file name of target image
+        position: position of the target in world coordinates ((x, y, z); meters)
+        size_in_meters: size of the target ((length, width); meters)
+        """
+        self.target_image = cv2.cvtColor(cv2.imread(image_file), cv2.COLOR_BGR2GRAY)
+
+        # Compute target corners in pixel (2D) coordinates (clockwise from top-left)
+        size_in_pixels = self.target_image.shape
+        corners = numpy.array((
+            (0, 0),
+            (1, 0),
+            (1, 1),
+            (0, 1),
+        )) * size_in_pixels
+
+        # NB: cv2.getPerspectiveTransform() requires CV_32F data type
+        self.target_corners_pixel = corners.astype(numpy.float32)
+
+        # Compute target corners in world (3D) coordinates (clockwise from top-left)
+        corners = numpy.array((
+            (-.5,  .5, 0),
+            ( .5,  .5, 0),
+            ( .5, -.5, 0),
+            (-.5, -.5, 0),
+        )) * size_in_meters + position
+
+        # Convert to homogeneous coordinates (as matrix of column vectors)
+        self.target_corners_world = numpy.vstack((numpy.matrix(corners.T), numpy.ones(4)))
+
+    def get_frame(self):
+        """
+        Generate a simulated camera frame.
+        """
+        # Get euler angles from vehicle pose
+        o = self.orientation
+        q = [o.x, o.y, o.z, o.w]
+        roll, pitch, yaw = euler_from_quaternion(q)
+
+        # Compute rotation matrix:
+        #   - apply rigid body rotations around body axes, in yaw-pitch-roll order,
+        #     to compute the camera's pose in world coordinates
+        #   - take the inverse (transpose) rotation matrix to map from world
+        #     to camera coordinates
+        # NB: Not sure why the angles need to be negated here, but mavros already
+        #     negates yaw and pitch, so we only need to negate roll (but why?!)
+        R = numpy.matrix(euler_matrix(yaw, pitch, -roll, axes="rzxy")).T
+
+        # Compute translation vector
+        # NB: We negate z because in the camera's frame, positive z is down
+        p = self.position
+        c = numpy.matrix((p.x, p.y, -p.z, 1)).T
+        t = -R * c
+
+        # Project target corners from world to (homogeneous) image coordinates
+        corners = self.P * (R * self.target_corners_world + t)
+
+        # Recover 2D points from homogeneous coordinates (as row vectors)
+        corners = (corners / corners[2])[:2].T
+
+        # NB: cv2.getPerspectiveTransform() requires CV_32F data type
+        corners = corners.astype(numpy.float32)
+
+        # Warp image
+        M = cv2.getPerspectiveTransform(self.target_corners_pixel, corners)
+        frame = cv2.warpPerspective(self.target_image, M, self.frame_size)
+
+        # Simulate frame rate
+        rospy.sleep(1.0 / self.frame_rate)
+
+        return frame
