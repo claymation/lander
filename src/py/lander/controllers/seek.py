30f2c8b382aa6ac301b6de2717e04e63103182b0
Clay McClure <clay@daemons.net>
2015-03-14 19:26:56 -0400

    seek: transition when target is tracked

--- src/py/lander/controllers/seek.py
+++ src/py/lander/controllers/seek.py
@@ -24,16 +24,9 @@ class SeekController(Controller):
     The general strategy here is to leverage the autopilot's waypoint navigation
     functionality by setting a position setpoint and letting the autopilot take
     us there. The tracker node, which is constantly surveiling the ground for the
-    landing target, should start to provide target error estimates once we're
+    landing target, should start to provide target tracking estimates once we're
     in proximity to the target. We'll use that as an indication that it's time
     to transition to the DESCEND state of the flight program.
-
-    For now, we have a static landing target position, so setting a location
-    setpoint at 10 Hz is like beating a dead horse like beating a dead horse
-    like beating a dead horse like -- you get the idea. But eventually we'll
-    be receiving dynamic updates of the target position, and then this will
-    be more useful. Besides, PX4 requires a constant stream of position msgs
-    for heartbeat to prevent an offboard failsafe.
     """
 
     def __init__(self, *args, **kwargs):
@@ -44,20 +37,25 @@ class SeekController(Controller):
         self.target_seek_altitude = rospy.get_param("target_seek_altitude",
                 DEFAULT_TARGET_SEEK_ALTITUDE)
 
-        rospy.Subscriber("/tracker/error",
-                geometry_msgs.msg.Point,
-                self.handle_error_message)
-
-    def handle_error_message(self, msg):
+    def handle_track_message(self, msg):
         """
-        Transition to DESCEND state when we start receiving target error messages.
+        Transition to DESCEND state when we start tracking the target.
         """
-        # TODO: Find a better way to transition between states
-        # TODO: Require a minimum certainty (covariance) before transitioning
-        if self.commander.state == FlightState.SEEK:
+        if msg.tracking.data:
             self.commander.transition_to_state(FlightState.DESCEND)
 
     def run(self):
+        """
+        Publish location setpoint once per control loop.
+
+        For now, we have a static landing target position, so repeatedly setting
+        the same location setpoint is like beating a dead horse like beating a
+        dead horse like beating a dead horse like -- you get the idea.
+
+        But eventually we'll receive dynamic updates of the target position, and
+        then this will be more useful. Besides, PX4 requires a constant stream of
+        heartbeat messages, so this is not for naught.
+        """
         # Construct an (x, y, z, yaw) setpoint, in local coordinates
         # NB: yaw currently has no effect (with ArduCopter, at least)
         x, y, z = self.target_local_position
