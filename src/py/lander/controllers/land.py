65930473a97f62e61625e699cd7886cfc6cc03e7
Clay McClure <clay@daemons.net>
2015-03-17 16:48:15 -0400

    land: request FCU to land

--- src/py/lander/controllers/land.py
+++ src/py/lander/controllers/land.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python
+# vim: set ts=4 sw=4 et:
+
 from lander.lib.controller import Controller
 
 
 class LandController(Controller):
     """
     Land on the landing target.
+
+    Landing is tricky. Ground effect causes random perturbations in
+    vehicle velocity, and we can't correct for that very well because
+    we're so close to the ground we don't have much room to maneuver
+    or margin for error.
+
+    So we just ask the FCU to land and then disarm the motors.
     """
-    def run(self):
-        pass
+    def enter(self):
+        self.vehicle.set_mode("LAND")
